9573c78a65aaa15d23be31e35d31820965b776b7
basilbabu0208 <159798923+basilbabu0208@users.noreply.github.com>
2024-07-25 10:24:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import validation 
+import emoji
+from streamlit_js_eval import streamlit_js_eval
+
+
+
+
+st.markdown("<h1 style='text-align: center;'>File Validation App</h1>", unsafe_allow_html=True)
+
+
+template = st.file_uploader("Upload Template File")
+real_file = st.file_uploader("Upload Real File")
+
+if st.button("Validate"):
+      if template is None:
+           st.write("Please upload a valid template file.")
+      elif real_file is None:
+           st.write("Please upload a valid real file.")
+      elif template and real_file:
+           validation.val(template, real_file)
+           
+if st.button("Home"):
+   # Reset session state and rerun the app
+
+   #validation.reset_app()
+   streamlit_js_eval(js_expressions = "parent.window.location.reload()")
+
+
