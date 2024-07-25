f3e02ef7c9ef22809028799496f22d88e27fbe1a
basilbabu0208 <159798923+basilbabu0208@users.noreply.github.com>
2024-07-25 11:47:07 +0530

    Update file_validation.py

--- file_validation.py
+++ file_validation.py
@@ -3,34 +3,26 @@ import pandas as pd
 import emoji
 from streamlit_js_eval import streamlit_js_eval
 
-
-
 def val(template, real_file):
-    st.write(pd.read_excel(template))
-
-
-
+    try:
+        df_template = pd.read_excel(template)
+        st.write(df_template)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
 st.markdown("<h1 style='text-align: center;'>File Validation App</h1>", unsafe_allow_html=True)
 
-
-template = st.file_uploader("Upload Template File")
-real_file = st.file_uploader("Upload Real File")
+template = st.file_uploader("Upload Template File", type=["xlsx"])
+real_file = st.file_uploader("Upload Real File", type=["xlsx"])
 
 if st.button("Validate"):
-      if template is None:
-           st.write("Please upload a valid template file.")
-      elif real_file is None:
-           st.write("Please upload a valid real file.")
-      elif template and real_file:
-           val(template, real_file)
+    if template is None:
+        st.write("Please upload a valid template file.")
+    elif real_file is None:
+        st.write("Please upload a valid real file.")
+    else:
+        val(template, real_file)
            
 if st.button("Home"):
-   # Reset session state and rerun the app
-
-   #validation.reset_app()
-   streamlit_js_eval(js_expressions = "parent.window.location.reload()")
-
-
-
-
+    # Use Streamlit's rerun functionality to reset app state
+    st.experimental_rerun()
